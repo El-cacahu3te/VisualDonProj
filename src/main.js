1017f9c773fc3c58023652bbf6813c71089f52df
El-cacahu3te <elia.nicolo@outlook.com>
2026-04-28 16:45:13 +0200

    start sport.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './style.css'
-import toys from './data/toys.json'
+
 // import sport from './sport.js'
 // import workplace from './workplace.js'
 // import feminicide from './feminicide.js'
 
+import { initSportChart } from './section/sport.js';
+initSportChart();
