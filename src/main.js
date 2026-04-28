cbf0cd01c677d63cff76a5a1a893586267114dd2
Chlopinette <chloe.muehlemann@outlook.com>
2026-04-28 16:16:53 +0200

    Ajout js pour toys

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './style.css'
 import toys from './data/toys.json'
+import sport from './data/sport.json'
+import workplace from './data/workplace.json'
+import feminicide from './data/feminicide.json'
 // import sport from './sport.js'
 // import workplace from './workplace.js'
 // import feminicide from './feminicide.js'
